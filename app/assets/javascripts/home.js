6c5729da61552b309a734fc2148f824f526bc280
Alajuwon Thomas <spooda7@yahoo.com>
2018-11-25 21:57:13 -0500

    added new nav and colors, content wise

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,3 +2,14 @@ $('#toggle').click(function() {
   $('.circle-loader').toggleClass('load-complete');
   $('.checkmark').toggle();
 });
+
+  $(document).ready(function(){
+
+     var checkScrollBar = function(){
+       $('#tip-top-nav').css({
+         backgroundColor: $(this).scrollTop() > 1 ?
+           '#fd4437' : 'transparent'
+       })
+     }
+     $(window).on('load resize scroll', checkScrollBar)
+     });
