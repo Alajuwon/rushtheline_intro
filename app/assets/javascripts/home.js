14544f6c302e0a25eaa978bb5da6d03a55c49b59
Alajuwon Thomas <spooda7@yahoo.com>
2018-11-28 22:32:05 -0500

    added transparency and different colors to home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,9 @@
-$('#toggle').click(function() {
-  $('.circle-loader').toggleClass('load-complete');
-  $('.checkmark').toggle();
-});
-
   $(document).ready(function(){
 
      var checkScrollBar = function(){
        $('#tip-top-nav').css({
          backgroundColor: $(this).scrollTop() > 1 ?
-           '#fd4437' : 'transparent'
+           '#b80720' : 'transparent'
        })
      }
      $(window).on('load resize scroll', checkScrollBar)
